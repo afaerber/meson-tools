cfe9c03404e69e307b796af64b8b111f631d775b
Andreas Färber <andreas.faerber@web.de>
2017-05-01 10:02:35 +0200

    Split aml.h from meson.h
    Move the AmlogicHeader struct and signature define to their own header.
    This clearly separates the MIT'ed data structures from any future GPL'ed
    helper functions.
    
    Signed-off-by: Andreas Färber <andreas.faerber@web.de>

--- meson.h
+++ meson.h
@@ -1,15 +1,14 @@
 /*
  * Copyright (c) 2017 Andreas Färber
  *
- * SPDX-License-Identifier: GPL-2.0+ OR MIT
+ * SPDX-License-Identifier: GPL-2.0+
  */
 
 #ifndef MESON_COMMON_H
 #define MESON_COMMON_H
 
-#include <stdint.h>
-
 #include "fip.h"
+#include "aml.h"
 
 #ifndef MIN
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
@@ -18,27 +17,5 @@
 #ifndef ROUND_UP
 #define ROUND_UP(x, a) (((x) + ((a) - 1)) & ~((a) - 1))
 #endif
-
-#define AMLOGIC_SIGNATURE "@AML"
-
-struct __attribute((__packed__)) AmlogicHeader {
-	char sig[4];
-	uint32_t size;
-	uint16_t header_size;
-	uint16_t header_version;
-	uint32_t id;
-	uint32_t encrypted;
-	uint32_t digest_offset;
-	uint32_t digest_size;
-	uint32_t data_offset;
-	uint32_t data_size;
-	uint32_t padding_offset;
-	uint32_t padding_size;
-	uint32_t _offset2;
-	uint32_t pad2;
-	uint32_t payload_offset;
-	uint32_t payload_size;
-	uint32_t unknown;
-};
 
 #endif
